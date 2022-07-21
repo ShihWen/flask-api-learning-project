ac88222d95cf42f61a48185beb6a5e783f7ff839
Peter Wu <shihwenwutw@gmail.com>
2022-07-22 07:21:15 +0800

    Fix not importing db when running the app.

--- app.py
+++ app.py
@@ -22,11 +22,6 @@ app.config['SQLAKCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'apple'
 api = Api(app)
 
-
-@app.before_first_request
-def create_table():
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Store, '/store/<string:name>')
