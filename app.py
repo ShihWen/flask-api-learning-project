31e8547d2f22be1f47744058f6a6338e4d743b1b
Peter Wu <shihwenwutw@gmail.com>
2022-07-24 09:05:40 +0800

    Fix postgres url for SQLAlchemy 14.x

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 import os
-
+import re
 
 from security import authenticate
 from security import identity
@@ -15,7 +15,13 @@ from resources.store import StoreList
 
 from db import db
 
-# hello~
+# Reference: 
+# https://help.heroku.com/ZKNTJQSK/why-is-sqlalchemy-1-4-x-not-connecting-to-heroku-postgres
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
